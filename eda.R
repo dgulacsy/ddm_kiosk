b9a293a18c4b1b2e5da96ef9b948ad0b507c5f67
Attila Szuts <attila.szuts@brokerchooser.com>
2021-01-27 22:50:53 +0100

    restore to dominik's version

--- eda.R
+++ eda.R
@@ -10,161 +10,201 @@ source("sum_stat.R")
 
 rm(list=ls())
 
-library(tidyverse)
-library(readxl)
-library(janitor)
+path<-"/Users/Dominik/OneDrive - Central European University/2nd_trimester/DDM/ddm_kiosk/"
 
-# read data
-fname <- 'data/Czech newspaperkiosk case dataset 20210104_v1.0.xlsx'
-turnover <- readxl::read_excel(fname, sheet = 'Turnover') %>% janitor::clean_names() %>% mutate(store_name = as.factor(store_name))
-store <- readxl::read_excel(fname, sheet = 'Store data') %>% janitor::clean_names() 
-costs <- readxl::read_excel(fname, sheet = 'Costs') %>% janitor::clean_names() %>% mutate(costs = -costs)
+# Read in data ------------------------------------------------------------
+fname <- 'data/raw/Czech newspaperkiosk case dataset 20210104_v1.0.xlsx'
+turnover <- read_xlsx(fname, sheet = 'Turnover') %>% clean_names()
+stores <- read_xlsx(fname, sheet = 'Store data') %>% clean_names() 
+cost <- read_xlsx(fname, sheet = 'Costs') %>% clean_names()
 
-# AVG performance YoY
-turnover_avg <- turnover %>% 
-  group_by(store_name, product_category, year) %>% 
+write_csv(turnover,paste0(path,"data/clean/turnover.csv"))
+write_csv(stores,paste0(path,"data/clean/stores.csv"))
+write_csv(cost,paste0(path,"data/clean/cost.csv"))
+
+# Data cleaning -----------------------------------------------------------
+str(stores)
+str(turnover)
+str(cost)
+
+# convert store_name column to string
+turnover$store_name <- as.character(turnover$store_name)
+cost$store_name <- as.character(cost$store_name)
+stores$store_name <- as.character(stores$store_name)
+
+# convert nominal variables to factors
+stores$prague<-as.factor(stores$prague)
+stores$price_category<- as.factor(stores$price_category)
+stores$lottery_pos<- as.factor(stores$lottery_pos)
+stores$pudo_pos<- as.factor(stores$pudo_pos)
+
+# convert numeric variables stored as character to numeric
+stores$sunday_opening_hours <- as.numeric(stores$sunday_opening_hours)
+stores$real_estate_price_sqm <- as.numeric(stores$real_estate_price_sqm)
+stores$x0_50m_avg <- as.numeric(stores$x0_50m_avg)
+stores$x50_100m_avg <- as.numeric(stores$x50_100m_avg)
+stores$x100_plusm_avg <- as.numeric(stores$x100_plusm_avg)
+stores$newspaper_shelves <- as.numeric(stores$newspaper_shelves)
+stores$non_newspaper_shelves <- as.numeric(stores$non_newspaper_shelves)
+
+# replace #missing values with NA
+stores[stores == "#MISSING"] <- NA
+
+# change sign of costs
+cost$costs <- -cost$costs
+
+# change unit of measurement for gm, cost
+cost$costs <- cost$costs/10^5
+turnover$gm <- turnover$gm/10^5
+
+# Create Year-Store Aggregated Dataset ------------------------------------
+# Aggregate data
+ys_t <- turnover %>% group_by(year,store_name) %>% 
+  summarise(
+    gm = sum(gm)
+  )
+
+# Join cost and turnover
+ys <- left_join(ys_t,cost,by=c("year","store_name"))
+
+# Join df and store data
+ys <- left_join(ys,stores, by="store_name")
+
+# Calculate net margin (Net margin = Gross margin - Non-COGS costs)
+ys$nm <- ys$gm-ys$costs
+
+# Create Year-Store-ProductCat Aggregated Dataset ------------------------
+ysp_t <- turnover %>% group_by(year,store_name, product_category) %>% 
+  summarise(
+    gm = sum(gm)
+  )
+
+ysp <- left_join(ysp_t,stores,by="store_name")
+
+# Create Year-Store-ProductCat-Month level Dataset ------------------------
+yspm <- left_join(turnover, stores, by="store_name")
+
+# General EDA ----------------------------------------------------------------------
+
+p1<-stores %>%
+  keep(is.numeric) %>% 
+  gather() %>% 
+  ggplot(aes(value)) +
+  facet_wrap(~key, scales = "free",ncol = 3) +
+  geom_histogram()+
+  theme_bw() + 
+  scale_fill_wsj()
+
+p1
+
+ggsave(p1, filename = paste0(path,"out/store_histograms.png"),height = 10, width = 5)
+
+# Descriptive statistics
+dstats<-sum_stat(stores, stores %>%
+           keep(is.numeric) %>% 
+           colnames(),
+         c('mean','median','min','max','1st_qu.','3rd_qu','sd','range'),
+         num_obs = F
+         ) %>% 
+        left_join(sum_stat(turnover,
+                           c("gm"),
+                           c('mean','median','min','max','1st_qu.','3rd_qu','sd','range'),
+                           num_obs = F),
+                  by="statistics") %>%
+        left_join(sum_stat(cost,
+                           c("costs"),
+                           c('mean','median','min','max','1st_qu.','3rd_qu','sd','range'),
+                           num_obs = F),
+                  by="statistics")
+
+# Distributions of GM by product categories and years
+ggplot(ysp_t, aes(gm)) + geom_histogram(position = 'dodge') + facet_grid(year ~ product_category,scales = 'free')
+
+# Distributions of GM by year
+ggplot(ys, aes(gm)) + geom_histogram(position = 'dodge') + facet_grid(scales = 'free', rows = . ~ year)
+
+# Distributions of Non-COGS Costs by years
+ggplot(cost, aes(costs)) + geom_histogram(position = 'dodge') + facet_grid(scales = 'free', rows = . ~ year)
+
+# Cost - Gross Margin Scatterplot
+suspicous <- subset(ys, costs<0)
+
+ggplot(ys , aes(x = costs, y = gm)) +
+  geom_point() +
+  geom_point(data=suspicous, colour="red")+
+  geom_text(data=suspicous, label="Error?", vjust=-1, hjust=0.25, color="red")+
+  theme_bw() +
+  geom_smooth(method="loess" , formula = y ~ x ) +
+  labs( title = "Relationship between Non-COGS Costs and Gross Margin",
+        y = "Gross Margin (100K)",
+        x = "Costs (100K)")+ 
+  facet_grid(scales = 'free', rows = . ~ year)
+
+# Select This year
+ys <- filter(ys, year=="This year")
+ysp <- filter(ysp, year=="This year")
+yspm <- filter(yspm, year=="This year")
+
+# Weekend Opening Hours ------------------------
+
+ggplot(ys, aes(x = round(sunday_opening_hours,1), y = gm)) +
+  geom_point() +
+  theme_bw() +
+  geom_smooth(method="loess" , formula = y ~ x )+
+  labs( title = "Pattern of association between Gross Margin and Sunday Opening Hours",
+        y = "Gross Margin (100K)",
+        x = "Sunday Opening Hours")
+
+# Create Sunday open dummy variable
+ys$sunday_open<-as.factor(ifelse(ys$sunday_opening_hours>0,1,0))
+yspm$sunday_open<-as.factor(ifelse(yspm$sunday_opening_hours>0,1,0))
+
+gm_s <- ys %>% group_by(sunday_open) %>% 
   summarise(
     avg_gm = mean(gm)
   )
-ggplot(turnover_avg, aes(year, avg_gm)) + geom_boxplot(position = 'dodge') + facet_grid(scales = 'free', rows = . ~ product_category)
 
-# AVG Monthly performance by category 
-turnover_mon <- turnover %>% 
-  mutate(year = ifelse(year == "This year", "1", "0")) %>% 
-  group_by(year, month, product_category) %>% 
+ggplot(gm_s, aes(fill=sunday_open, y=avg_gm, x=sunday_open)) + 
+  geom_bar(position="dodge", stat="identity")
+
+gm_mps<- yspm %>% group_by(product_category,month,sunday_open) %>% 
   summarise(
     avg_gm = mean(gm)
-  ) %>% 
-  arrange(year, month, product_category)
-
-grouping <- rep(seq(1, 60), 2)
-turnover_mon['grouping'] <- grouping
-
-ggplot(turnover_mon, aes(year, avg_gm)) + geom_point() + facet_grid(scales = 'free', rows = vars(product_category), cols = vars(month)) + geom_line(aes(group = grouping))
-
-==== BASE ====
-rm(description)
-==== BASE ====
-
-# join all tables together for modelling
-df <- turnover %>% 
-  left_join(costs, by = c('store_name', 'year')) %>% 
-  left_join(store, by = c('store_name')) %>% 
-  mutate(year = fct_recode(as.factor(year), "0" = "Last year", "1" = "This year"))
-
-# calculate NM ( = GM + NCOGS)
-df <- df %>% 
-  group_by(year, store_name) %>% 
-  summarise(
-    gm_y = sum(gm)
-  ) %>% 
-  right_join(df, by = c('year', 'store_name')) %>% 
-  mutate(
-    nm = gm_y - costs
   )
 
-# Shops that are underperformers ------------------------------------------
-# create yearly aggregated average net-margin variable
-nm_avg_y <- df %>% 
-  group_by(year) %>% 
+ggplot(gm_mps, aes(fill=sunday_open, y=avg_gm, x=sunday_open)) + 
+  geom_bar(position="dodge", stat="identity") + 
+  facet_grid(product_category ~ month,scales = 'free')
+
+# PUDO Services -----------------------------------------------------------
+gm_p<- yspm %>% group_by(product_category,month) %>% 
   summarise(
-    avg_nm = mean(nm)
+    avg_gm = mean(gm)
   )
 
-nm_avg_0 <- as.numeric(nm_avg_y[1, 2])
-nm_avg_1 <- as.numeric(nm_avg_y[2, 2])
+ggplot(gm_p, aes(fill=product_category, y=avg_gm, x=product_category)) + 
+  geom_bar(position="dodge", stat="identity") + 
+  facet_grid(product_category ~ month,scales = 'free')+
+  theme(axis.text.x=element_blank(),
+        legend.position = "none")
 
-# filter based on yearly nm 
-df_up <- df %>% 
-  mutate(
-    underperformer_0 = ifelse(year == 0 & nm < nm_avg_0, 1, ifelse(year == 1, NA, 0)),
-    underperformer_1 = ifelse(year == 1 & nm < nm_avg_1, 1, ifelse(year == 0, NA, 0))
-  )
+# Newspaper Focus (newspaper focus ratio) --------
+# calculate newspaper ratio
+ys$npr<- ys$newspaper_shelves/(ys$newspaper_shelves+ys$non_newspaper_shelves)
 
-df_up %>% 
-  summarise(
-    underperformers_last_year = sum(underperformer_0, na.rm = T),
-    underperformers_this_year = sum(underperformer_1, na.rm = T),
-    total = n(),
-    perc_total_0 = underperformers_last_year / total * 100,
-    perc_total_1 = underperformers_this_year / total * 100
-  )
+ggplot(ys, aes(x = npr, y = gm)) +
+  geom_point() +
+  theme_bw() +
+  geom_smooth(method="loess" , formula = y ~ x )+
+  labs( title = "Pattern of association between Gross Margin and Newspaper focus ratio",
+        y = "Gross Margin (100K)",
+        x = "Newspaper focus ratio")
 
-# histogram of nm and gm
-df_hist <- df %>% group_by(year, store_name) %>% summarise(gm = mean(gm), nm = mean(nm), costs = mean(costs))
+ggplot(ys, aes(x = npr, y = nm)) +
+  geom_point() +
+  theme_bw() +
+  geom_smooth(method="loess" , formula = y ~ x )+
+  labs( title = "Pattern of association between Gross Margin and Newspaper focus ratio",
+        y = "Net Margin (100K)",
+        x = "Newspaper focus ratio")
 
-df_hist %>% ggplot(aes(nm)) + geom_histogram(bins = 50) + geom_vline(xintercept = median(df_hist$nm), color = "red")
-df_hist %>% ggplot(aes(gm)) + geom_histogram(bins = 50)
-
-# scatter plot of costs ~ nm/gm
-df_hist %>% ggplot(aes(nm, costs)) + geom_point()
-df_hist %>% ggplot(aes(gm, costs)) + geom_point()
-
-# gambling ----------------------------------------------------------------
-# create total gross-margin and total gross-margin per category
-df_g <- df %>% 
-  group_by(store_name, product_category, year) %>% 
-  summarise(
-    total_gm_cat = sum(gm)
-  ) %>% 
-  right_join(df, by = c("store_name", "product_category", "year"))
-df_g <- df_g %>% 
-  group_by(store_name, year) %>% 
-  summarise(
-    total_gm = sum(gm)
-  ) %>% 
-  right_join(df_g, by = c("store_name", "year"))
-
-# calculate percentage of total gross margin per category
-df_g <- df_g %>% 
-  group_by(store_name, year, product_category) %>% 
-  summarise(
-    total_gm_cat_perc = total_gm_cat / total_gm * 100
-  ) %>% 
-  right_join(df_g, by = c("store_name", "year", "product_category"))
-
-df_g <- df_g[!duplicated(df_g),]
-
-
-# create average perc of total gm per category per year
-prod_cat_becnh <- df_g %>% 
-  group_by(year, product_category) %>% 
-  summarise(
-    avg_perc_gm = mean(total_gm_cat_perc)
-  ) %>% 
-  arrange(year, desc(avg_perc_gm)) %>% 
-  mutate(avg_perc_gm = paste0(round(avg_perc_gm, 2), " %"),
-         year = ifelse(year == 0, "Last year", "This year")) %>% 
-  rename("Year" = "year", "Product Category" = "product_category", "Average Gross Margin (%)"="avg_perc_gm")
-
-# count number of high impact gambling stores per year
-df_g %>% 
-  filter(product_category == "Sazka") %>% 
-  mutate(gambling_high_imp = ifelse(total_gm_cat_perc > 25, 1, 0)) %>% 
-  group_by(store_name, year) %>% 
-  summarise(
-    gambl_hi_count = mean(gambling_high_imp)
-  ) %>% 
-  group_by(year) %>% 
-  summarise(
-    total_hi_gamb = sum(gambl_hi_count)
-  )
-
-# create high impact gambling dummy
-df_g <- df_g %>% 
-  mutate(gamb_hi_imp = ifelse(product_category == "Sazka" & total_gm_cat_perc > 25, 1, ifelse(product_category != "Sazka", NA, 0))) 
-
-# seasonality -------------------------------------------------------------
-
-turnover_mon <- turnover_mon %>% 
-  arrange(year, product_category, month) 
-
-grouping_mon <- c(rep(1, 12), rep(2, 12), rep(3, 12), rep(4, 12), rep(5, 12), rep(6, 12), rep(7, 12), rep(8, 12), rep(9, 12), rep(10, 12))
-
-turnover_mon['grouping_mon'] <- grouping_mon
-
-turnover_mon %>% ggplot(aes(month, avg_gm)) + facet_grid(rows = vars(product_category), cols = vars(year), scales = "free") + geom_point() + geom_line(aes(group = grouping_mon))
-
-# sales performance factors per product category --------------------------
-
-glimpse(df)
