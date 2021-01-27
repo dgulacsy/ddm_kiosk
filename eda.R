68d4be72a6fa1a1f251edde1cbffbd9c430414f9
gulacsydominik@gmail.com <dgulacsy>
2021-01-27 18:02:41 +0100

    update rmd

--- eda.R
+++ eda.R
@@ -90,14 +90,14 @@ p1<-stores %>%
   keep(is.numeric) %>% 
   gather() %>% 
   ggplot(aes(value)) +
-  facet_wrap(~key, scales = "free",ncol = 3) +
+  facet_wrap(~key, scales = "free",ncol = 4) +
   geom_histogram()+
   theme_bw() + 
   scale_fill_wsj()
 
 p1
 
-ggsave(p1, filename = paste0(path,"out/store_histograms.png"),height = 10, width = 5)
+ggsave(p1, filename = paste0(path,"out/store_histograms.png"),height = 5, width = 8)
 
 # Descriptive statistics
 dstats<-sum_stat(stores, stores %>%
@@ -118,18 +118,24 @@ dstats<-sum_stat(stores, stores %>%
                   by="statistics")
 
 # Distributions of GM by product categories and years
-ggplot(ysp_t, aes(gm)) + geom_histogram(position = 'dodge') + facet_grid(year ~ product_category,scales = 'free')
+p2<-ggplot(ysp_t, aes(gm)) + geom_histogram(position = 'dodge') + facet_grid(year ~ product_category,scales = 'free')
+p2
+ggsave(p2, filename = paste0(path,"out/gm_prod_histograms.png"),height = 5, width = 10)
 
 # Distributions of GM by year
-ggplot(ys, aes(gm)) + geom_histogram(position = 'dodge') + facet_grid(scales = 'free', rows = . ~ year)
+p3<-ggplot(ys, aes(gm)) + geom_histogram(position = 'dodge') + facet_grid(scales = 'free', rows = . ~ year)
+p3
+ggsave(p3, filename = paste0(path,"out/gm_histograms.png"),height = 5, width = 10)
 
 # Distributions of Non-COGS Costs by years
-ggplot(cost, aes(costs)) + geom_histogram(position = 'dodge') + facet_grid(scales = 'free', rows = . ~ year)
+p4<-ggplot(cost, aes(costs)) + geom_histogram(position = 'dodge') + facet_grid(scales = 'free', rows = . ~ year)
+p4
+ggsave(p4, filename = paste0(path,"out/cost_histograms.png"),height = 5, width = 10)
 
 # Cost - Gross Margin Scatterplot
 suspicous <- subset(ys, costs<0)
 
-ggplot(ys , aes(x = costs, y = gm)) +
+p5<-ggplot(ys , aes(x = costs, y = gm)) +
   geom_point() +
   geom_point(data=suspicous, colour="red")+
   geom_text(data=suspicous, label="Error?", vjust=-1, hjust=0.25, color="red")+
@@ -139,6 +145,8 @@ ggplot(ys , aes(x = costs, y = gm)) +
         y = "Gross Margin (100K)",
         x = "Costs (100K)")+ 
   facet_grid(scales = 'free', rows = . ~ year)
+p5
+ggsave(p5, filename = paste0(path,"out/cost_gm_scatterplot.png"),height = 7, width = 10)
 
 # Select This year
 ys <- filter(ys, year=="This year")
@@ -146,35 +154,40 @@ ysp <- filter(ysp, year=="This year")
 yspm <- filter(yspm, year=="This year")
 
 # Weekend Opening Hours ------------------------
-
-ggplot(ys, aes(x = round(sunday_opening_hours,1), y = gm)) +
+p6<-ggplot(ys, aes(x = round(sunday_opening_hours,1), y = nm)) +
   geom_point() +
   theme_bw() +
   geom_smooth(method="loess" , formula = y ~ x )+
-  labs( title = "Pattern of association between Gross Margin and Sunday Opening Hours",
-        y = "Gross Margin (100K)",
+  labs( title = "Pattern of association between Net Margin and Sunday Opening Hours",
+        y = "Net Margin (100K)",
         x = "Sunday Opening Hours")
+p6
+ggsave(p6, filename = paste0(path,"out/sunday_scatterplot.png"),height = 7, width = 10)
 
 # Create Sunday open dummy variable
 ys$sunday_open<-as.factor(ifelse(ys$sunday_opening_hours>0,1,0))
 yspm$sunday_open<-as.factor(ifelse(yspm$sunday_opening_hours>0,1,0))
 
-gm_s <- ys %>% group_by(sunday_open) %>% 
+nm_s <- ys %>% group_by(sunday_open) %>% 
   summarise(
-    avg_gm = mean(gm)
+    avg_nm = mean(nm)
   )
 
-ggplot(gm_s, aes(fill=sunday_open, y=avg_gm, x=sunday_open)) + 
+p7<-ggplot(nm_s, aes(fill=sunday_open, y=avg_nm, x=sunday_open)) + 
   geom_bar(position="dodge", stat="identity")
+p7
+ggsave(p7, filename = paste0(path,"out/sunday_bars.png"),height = 5, width = 5)
 
 gm_mps<- yspm %>% group_by(product_category,month,sunday_open) %>% 
   summarise(
     avg_gm = mean(gm)
   )
 
-ggplot(gm_mps, aes(fill=sunday_open, y=avg_gm, x=sunday_open)) + 
+p8<-ggplot(gm_mps, aes(fill=sunday_open, y=avg_gm, x=sunday_open)) + 
   geom_bar(position="dodge", stat="identity") + 
   facet_grid(product_category ~ month,scales = 'free')
+p8
+ggsave(p8, filename = paste0(path,"out/sunday_prod_month_bars.png"),height = 5, width = 7)
 
 # PUDO Services -----------------------------------------------------------
 gm_p<- yspm %>% group_by(product_category,month) %>% 
@@ -182,29 +195,35 @@ gm_p<- yspm %>% group_by(product_category,month) %>%
     avg_gm = mean(gm)
   )
 
-ggplot(gm_p, aes(fill=product_category, y=avg_gm, x=product_category)) + 
+p9<-ggplot(gm_p, aes(fill=product_category, y=avg_gm, x=product_category)) + 
   geom_bar(position="dodge", stat="identity") + 
   facet_grid(product_category ~ month,scales = 'free')+
   theme(axis.text.x=element_blank(),
         legend.position = "none")
+p9
+ggsave(p9, filename = paste0(path,"out/pudo_prod_month_bars.png"),height = 5, width = 7)
 
 # Newspaper Focus (newspaper focus ratio) --------
 # calculate newspaper ratio
 ys$npr<- ys$newspaper_shelves/(ys$newspaper_shelves+ys$non_newspaper_shelves)
 
-ggplot(ys, aes(x = npr, y = gm)) +
+p10<-ggplot(ys, aes(x = npr, y = gm)) +
   geom_point() +
   theme_bw() +
   geom_smooth(method="loess" , formula = y ~ x )+
   labs( title = "Pattern of association between Gross Margin and Newspaper focus ratio",
         y = "Gross Margin (100K)",
         x = "Newspaper focus ratio")
+p10
+ggsave(p10, filename = paste0(path,"out/newspaper_ratio_gm_scatterplot.png"),height = 5, width = 5)
 
-ggplot(ys, aes(x = npr, y = nm)) +
+p11<-ggplot(ys, aes(x = npr, y = nm)) +
   geom_point() +
   theme_bw() +
   geom_smooth(method="loess" , formula = y ~ x )+
   labs( title = "Pattern of association between Gross Margin and Newspaper focus ratio",
         y = "Net Margin (100K)",
         x = "Newspaper focus ratio")
+p11
+ggsave(p11, filename = paste0(path,"out/newspaper_ratio_nm_scatterplot.png"),height = 5, width = 5)
 
