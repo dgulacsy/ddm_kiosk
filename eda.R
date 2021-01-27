81adf9187def156c1197f3ec4da987141194c78d
Attila Szuts <attila.szuts@brokerchooser.com>
2021-01-27 22:12:45 +0100

    add seasonality plots

--- eda.R
+++ eda.R
@@ -21,11 +21,17 @@ ggplot(turnover_avg, aes(year, avg_gm)) + geom_boxplot(position = 'dodge') + fac
 
 # AVG Monthly performance by category 
 turnover_mon <- turnover %>% 
+  mutate(year = ifelse(year == "This year", "1", "0")) %>% 
   group_by(year, month, product_category) %>% 
   summarise(
     avg_gm = mean(gm)
-  )
-ggplot(turnover_mon, aes(year, avg_gm)) + geom_boxplot(position = 'dodge') + facet_grid(scales = 'free', rows = vars(product_category), cols = vars(month))
+  ) %>% 
+  arrange(year, month, product_category)
+
+grouping <- rep(seq(1, 60), 2)
+turnover_mon['grouping'] <- grouping
+
+ggplot(turnover_mon, aes(year, avg_gm)) + geom_point() + facet_grid(scales = 'free', rows = vars(product_category), cols = vars(month)) + geom_line(aes(group = grouping))
 
 
 # join all tables together for modelling
@@ -75,12 +81,13 @@ df_up %>%
 # histogram of nm and gm
 df_hist <- df %>% group_by(year, store_name) %>% summarise(gm = mean(gm), nm = mean(nm), costs = mean(costs))
 
-df_hist %>% ggplot(aes(nm)) + geom_histogram(bins = 50)
+df_hist %>% ggplot(aes(nm)) + geom_histogram(bins = 50) + geom_vline(xintercept = median(df_hist$nm), color = "red")
 df_hist %>% ggplot(aes(gm)) + geom_histogram(bins = 50)
 
 # scatter plot of costs ~ nm/gm
 df_hist %>% ggplot(aes(nm, costs)) + geom_point()
 df_hist %>% ggplot(aes(gm, costs)) + geom_point()
+
 # gambling ----------------------------------------------------------------
 # create total gross-margin and total gross-margin per category
 df_g <- df %>% 
@@ -112,7 +119,11 @@ prod_cat_becnh <- df_g %>%
   group_by(year, product_category) %>% 
   summarise(
     avg_perc_gm = mean(total_gm_cat_perc)
-  ) 
+  ) %>% 
+  arrange(year, desc(avg_perc_gm)) %>% 
+  mutate(avg_perc_gm = paste0(round(avg_perc_gm, 2), " %"),
+         year = ifelse(year == 0, "Last year", "This year")) %>% 
+  rename("Year" = "year", "Product Category" = "product_category", "Average Gross Margin (%)"="avg_perc_gm")
 
 # count number of high impact gambling stores per year
 df_g %>% 
@@ -133,8 +144,15 @@ df_g <- df_g %>%
 
 # seasonality -------------------------------------------------------------
 
+turnover_mon <- turnover_mon %>% 
+  arrange(year, product_category, month) 
 
+grouping_mon <- c(rep(1, 12), rep(2, 12), rep(3, 12), rep(4, 12), rep(5, 12), rep(6, 12), rep(7, 12), rep(8, 12), rep(9, 12), rep(10, 12))
+
+turnover_mon['grouping_mon'] <- grouping_mon
+
+turnover_mon %>% ggplot(aes(month, avg_gm)) + facet_grid(rows = vars(product_category), cols = vars(year), scales = "free") + geom_point() + geom_line(aes(group = grouping_mon))
 
 # sales performance factors per product category --------------------------
 
-
+glimpse(df)
